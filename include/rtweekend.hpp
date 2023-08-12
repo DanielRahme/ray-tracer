935907e80c98dcf806055dd075a509e3a6cc3784
Daniel Rahme <daniel.rahme.dr@gmail.com>
2023-08-13 00:27:44 +0200

    Add anti-aliasing

---
+++ include/rtweekend.hpp
@@ -0,0 +1,47 @@
+#ifndef RTWEEKEND_HPP
+#define RTWEEKEND_HPP
+
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <memory>
+#include <random>
+
+// Usings
+
+// using std::make_shared;
+// using std::shared_ptr;
+// using std::sqrt;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi       = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees)
+{
+    return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max)
+{
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
+}
+
+// Common Headers
+
+#include "interval.hpp"
+#include "ray.hpp"
+#include "vec3.hpp"
+
+#endif
